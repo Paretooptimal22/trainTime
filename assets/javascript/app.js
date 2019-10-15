c5cc3e6f4e2035bc8af045cdcf98492d567c0634
Paretooptimal22 <you.k2208@gmail.com>
2019-10-14 20:45:54 -0700

    added comments

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,27 +1,26 @@
-
+// create database variable
 const db = firebase.firestore()
 
+// create data collection
 db.collection(`trains`)
 
-// // initial variables
-// let train,
-//   dest,
-//   time,
-//   freq
-
-
+// event listener to add train data to firebase
 document.getElementById(`submitBtn`).addEventListener(`click`, e => {
   
   e.preventDefault()
 
+  // create variable as object data from form inputs for firebase
   let trainData = {
     train: document.getElementById(`train`).value,
     destination: document.getElementById(`dest`).value,
     firstArrival: document.getElementById(`firstTime`).value,
     frequency: document.getElementById(`freqMin`).value
   }
+
+  // add train data from form to firebase
   db.collection(`trains`).add(trainData)
 
+  // clear form after data added to firebase
   document.getElementById(`train`).value = ``
   document.getElementById(`dest`).value = ``
   document.getElementById(`firstTime`).value = ``
